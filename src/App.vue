2939155d7e1e1dca0b50a61f0fb4c88b87eec75e
Hiromitsu Sasaki <sasaki.hiromitsu.eg65@members.co.jp>
2020-02-11 15:50:31 +0900

    Add watching location

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,18 @@ import Geolocation from './components/Geolocation.vue'
 
 export default {
   name: 'App',
+  head: {
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+    ],
+  },
   components: {
     Geolocation
   }
 }
 </script>
 
-<style>
+<style lang="sass">
+@import "../node_modules/bulma/bulma.sass";
 </style>
--- src/components/Geolocation.vue
+++ src/components/Geolocation.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <h1>geolocation sample</h1>
-  </div>
+  <section>
+    <div class="container">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <h1 style="margin-top: 24px;" class="is-size-2">geolocation sample</h1>
+          <div style="height: 128px;">
+            <p style="margin-top: 48px;">現在地: 緯度: {{lat}}  軽度: {{long}}</p>
+          </div>
+          <button type="button" class="button" v-on:click="watchLocation()">位置情報の取得を開始する</button>
+          <button style="margin-top:48px" type="button" class="button" v-on:click="finishLocation()" >位置情報の取得を終了する</button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Geolocation',
+  data: function () {
+    return {
+      long: 0,
+      lat: 0,
+      watchId: null,
+    }
+  },
+  methods: {
+    watchLocation: function () {
+      navigator.geolocation.watchPosition((position) => {
+        this.lat = position.coords.latitude
+        this.long = position.coords.longitude
+      })
+    },
+    finishLocation: function () {
+      navigator.geolocation.clearWatch(this.watchId);
+    }
+  }
 }
 </script>
 
